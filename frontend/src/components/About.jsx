b494bccc56c61ca99091355e195712eaf272548b
Fredheda <frederik.heda@gmx.net>
2024-11-24 22:09:11 +0000

    updated about me section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -15,13 +15,19 @@ const About = () => {
           <div className="flex-grow min-w-[280px] text-base text-black leading-[1.8] text-left">
             <div className="rounded-lg mb-4">
               <p>
-                <strong>Frederik</strong> is a senior data scientist at <strong>BP</strong>, where he spearheads digital innovation across the trading and shipping organisation. He is an <em>AI enthusiast</em> with a strong background in <strong>machine learning</strong>, <strong>statistics</strong>, and <strong>software engineering</strong>.
-                <br /><br />
-                Frederik's role is to drive innovation through advanced data science and analytics solutions, leading the development and adoption of <strong>generative AI</strong> across the trading and shipping organisation. He is a leading member of BP's <strong>data & analytics</strong> and <strong>GenAI communities of practice</strong>, actively demystifying data science and AI across the business.
-                <br /><br />
-                Frederik holds master's degrees in <strong>Mechanical & Electrical Engineering</strong> from <strong>Robert Gordon University</strong>, and <strong>Applied Data Science</strong> from the <strong>University of Buckingham</strong>, and has led projects across various domains including <strong>demand forecasting</strong>, <strong>causal inference</strong>, and <strong>generative AI</strong>.
-                <br /><br />
-                Frederik is a strong advocate for <em>democratising artificial intelligence</em> and believes it can help us be more efficient, productive, and safe.
+              I'm a <strong>senior machine learning engineer</strong> at <strong>BP</strong> 🚀, where I lead digital innovation across the trading and shipping organisation. I'm passionate about <em>AI</em> with a strong background in <strong>machine learning</strong>, <strong>statistics</strong>, and <strong>software engineering</strong>.
+              <br /><br />
+              <strong>My Role 🧑🏻‍💻</strong><br/>
+              My role focuses on driving innovation through advanced data science and analytics solutions, particularly leading the development and adoption of <strong>generative AI</strong> across the organisation. I'm an active leader in BP's <strong>data & analytics</strong> and <strong>GenAI communities of practice</strong>, working to make data science and AI more accessible across the business.<br/>
+              over the last two years, I have been leading the development and implementation of <strong>Retrieval Augmented Generation (RAG)</strong> solutions, which have been deployed across the organisation to improve efficiency and drive innovation.
+              Throughout my career, I've led projects across various domains including <strong>demand forecasting</strong>, <strong>causal inference</strong>, and <strong>generative AI</strong>.
+              <br /><br />
+              <strong>Academics 🎓</strong><br/>
+              I hold master's degrees in <strong>Mechanical & Electrical Engineering</strong> from <strong>Robert Gordon University</strong>, and <strong>Applied Data Science</strong> from the <strong>University of Buckingham</strong>. While doing my first Masters at RGU, I was also president of the University table tennis club and student representative for my cohort.
+              <br /><br />
+              <strong>Passions 🏓</strong><br/>
+              I'm a strong advocate for <em>democratising artificial intelligence</em> 🤝 and believe it can help us work smarter, not harder. I'm always excited to collaborate on innovative projects that push the boundaries of what's possible with AI.
+              <br/> In my free time, I enjoy playing <strong>table tennis</strong> 🏓, <strong>travelling</strong> 🌍, and <strong>learning new things</strong> 📚. I've also recently <em>"rediscovered"</em> e-books and am trying to listen to 1-2, mostly non-fiction, books every month.
               </p>
             </div>
           </div>
@@ -43,11 +49,17 @@ const About = () => {
           <div className="flex-grow min-w-[280px] text-base text-black leading-[1.8] text-left">
             <div className="rounded-lg mb-4">
               <p>
-                [Placeholder: Brief introduction about the website's purpose]
+                This Portfolio website was built to introduce myself and showcase my work. It was built using <strong>React</strong> and <strong>Tailwind CSS</strong>, and is hosted on <strong>Heroku</strong>.
+                <br/>
+                In the following sections, you can find more information about my <strong>skills</strong>, <strong>projects</strong>, and <strong>interests</strong>.
                 <br /><br />
-                [Placeholder: Technical stack and implementation details]
+                Say Hi to <strong>Fredbot 🤖</strong>! <br/>
+                Fredbot is a simple, chatbot that can answer questions about me and my work. <br /> Please be kind and patient when using Fredbot - it is still learning! 
                 <br /><br />
-                [Placeholder: Features and functionality overview]
+                <strong>Draft mode</strong><br />
+                This website is currently in draft mode and is being updated regularly. I am using this website to learn more about web development and to showcase my work.
+                Many parts of this site are still under construction, not optimised, or may contain placeholder content.
+                <br /> Please check back soon for more updates!
               </p>
             </div>
           </div>
